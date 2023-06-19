08bfb1462fe65402c647bed944f6633e84ab1303
ThinkPad_P53s <ThinkPad_P53s@DESKTOP-3PGJHFO>
2023-06-19 15:40:58 +0300

    Add project files.

---
+++ MVCIntroDemo/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCIntroDemo.Models;
+using System.Diagnostics;
+using System.Text.Json;
+using MVCIntroDemo.Models.Product;
+
+namespace MVCIntroDemo.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger <HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			ViewBag.Message = "Hello World!";
+			return View();
+		}
+
+		public IActionResult About() 
+		{
+			ViewBag.Message = "This is an ASP.NET Core MVC app";
+			return View();
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[HttpGet]
+		public IActionResult NumbersToN () 
+		{
+			ViewBag.Count = -1;
+			return this.View();
+		}
+
+		[HttpPost]
+		public IActionResult NumbersToN(int count = -1)
+		{
+			ViewBag.Count = count;
+			return this.View();
+		}
+
+		public IActionResult Numbers() 
+		{ 
+			return View();
+		}
+
+	
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ MVCIntroDemo/Controllers/ProductController.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
+using MVCIntroDemo.Models.Product;
+using System.Collections;
+using System.Text;
+using System.Text.Json;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace MVCIntroDemo.Controllers
+{
+	public class ProductController : Controller
+	{
+		[ActionName("My-Products")]
+		public IActionResult All(string keyword)
+		{
+			if (keyword != null)
+			{
+				var foundProduct = _products
+					.Where(p => p.Name
+					               .ToLower()
+								   .Contains(keyword.ToLower())
+					       );
+
+				return View(foundProduct);
+			}
+			return View(_products);
+		}
+
+		public IActionResult ById(int id)
+		{
+			var product = _products.FirstOrDefault(x => x.Id == id);
+
+			if (product == null) 
+			{
+				return NotFound();
+			}
+
+			return View(product);
+		}
+
+		public IActionResult AllAsJson()
+		{
+			var options = new JsonSerializerOptions
+			{
+				WriteIndented = true
+			};
+
+			return Json(_products, options);
+		}
+
+		public IActionResult AllAsText()
+		{
+			//var text = string.Empty;
+			//foreach (var product in _products)
+			//{
+			//	text += ($"Product {product.Id}: {product.Name} - {product.Price} lv. ");
+			//	text += Environment.NewLine;
+
+			//}
+			//return Content(text);
+
+			StringBuilder sb = new StringBuilder();
+			foreach (var product in _products)
+			{
+				sb.AppendLine ($"Product {product.Id}: {product.Name} - {product.Price} lv. ");
+			}
+
+			return Content(sb.ToString());
+
+		}
+
+		public IActionResult AllAsTextFile()
+		{
+			StringBuilder sb = new StringBuilder();
+			foreach (var product in _products)
+			{
+				sb.AppendLine($"Product {product.Id}: {product.Name} - {product.Price:f2} lv. ");
+			}
+
+			Response.Headers.Add(HeaderNames.ContentDisposition,
+				@"attachment;filename=products.txt");
+
+			return File(Encoding.UTF8.GetBytes(sb.ToString().TrimEnd()), "text/plain");
+		}
+
+
+
+		private IEnumerable<ProductViewModel> _products =
+			new List<ProductViewModel>()
+			{
+			new ProductViewModel()
+			{
+				Id = 1,
+				Name = "Cheese",
+				Price = 7.00
+			},
+
+			new ProductViewModel() 
+			{ 
+				Id = 2,
+				Name = "Ham",
+				Price = 5.50
+			},
+
+			new ProductViewModel()
+			{
+				Id = 3,
+				Name = "Bread",
+				Price = 1.50
+			}
+
+			};
+		
+	}
+}
